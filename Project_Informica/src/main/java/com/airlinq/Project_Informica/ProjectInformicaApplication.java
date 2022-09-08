d70484604fc48a24b0e0f7f23363bfcd5a30c03f
ankit-aq <ankit.sharma@airlinq.com>
2022-09-08 12:45:31 +0530

    1st commit

---
+++ Project_Informica/src/main/java/com/airlinq/Project_Informica/ProjectInformicaApplication.java
@@ -0,0 +1,13 @@
+package com.airlinq.Project_Informica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInformicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectInformicaApplication.class, args);
+	}
+
+}
---
+++ Project_Informica/src/test/java/com/airlinq/Project_Informica/ProjectInformicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinq.Project_Informica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInformicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
