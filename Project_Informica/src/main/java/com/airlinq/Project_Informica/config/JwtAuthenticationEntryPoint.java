90656bc33fff351086d46fe6b767cc12cec36df0
ankit-aq <ankit.sharma@airlinq.com>
2022-09-14 12:10:56 +0530

    Last commit

---
+++ Project_Informica/src/main/java/com/airlinq/Project_Informica/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.airlinq.Project_Informica.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * This JwtAuthenticationEntryPoint class returns the error codes if any exception occurs. 
+ * 
+ * @author ANkit Sharma
+ * @version 1.0
+ *
+ */
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+
+		response.sendError(401, "Unauthorized");
+	}
+
+}
